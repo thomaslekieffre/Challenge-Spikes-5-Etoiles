70e36e814381aa4da0b844b96f4b8048a2c0a254
Thomas Lekieffre <166929924+thomaslekieffre@users.noreply.github.com>
2024-06-23 15:29:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
+// Sélectionne tous les éléments avec la classe "star" et le bouton de réinitialisation par son ID
 const stars = document.querySelectorAll(".star");
 const btnReset = document.getElementById("resetBtn");
 
 // Variable pour stocker la note sélectionnée
 let selectedRating = 0;
 
-// Fonction pour gérer le survol de la souris
+// Fonction pour gérer le survol des étoiles
 function handleMouseOver(index) {
   // Change les étoiles jusqu'à celle survolée
-  for (let i = 0; i < stars.length; i++) {
+  stars.forEach((star, i) => {
     if (i <= index) {
-      stars[i].src = "./img/star_full.png";
+      star.src = "./img/star_full.png";
     }
     if (i === index) {
-      stars[i].src = "./img/star_hover.png";
+      star.src = "./img/star_hover.png";
     }
-  }
+  });
 }
 
-// Fonction pour gérer la sortie de la souris
+// Fonction pour gérer la sortie de la souris des étoiles
 function handleMouseOut() {
   // Si aucune note n'est sélectionnée, réinitialise les étoiles
   if (selectedRating === 0) {
-    stars.forEach((star) => {
+    stars.forEach(star => {
       star.src = "./img/star_empty.png";
     });
   } else {
-    // Si une note est sélectionnée, affiche les étoiles jusqu'à cette note
-    for (let i = 0; i < stars.length; i++) {
-      if (i < selectedRating) {
-        stars[i].src = "./img/star_pink.png";
-      } else {
-        stars[i].src = "./img/star_empty.png";
-      }
-    }
+    // Sinon, affiche les étoiles selon la note sélectionnée
+    stars.forEach((star, i) => {
+      star.src = i < selectedRating ? "./img/star_pink.png" : "./img/star_empty.png";
+    });
   }
 }
 
@@ -41,24 +38,21 @@ function handleClick(index) {
   // Fixe la note sélectionnée
   selectedRating = index + 1;
   // Change les étoiles jusqu'à celle cliquée
-  for (let i = 0; i < stars.length; i++) {
-    if (i < selectedRating) {
-      stars[i].src = "./img/star_pink.png";
-    } else {
-      stars[i].src = "./img/star_empty.png";
-    }
-  }
+  stars.forEach((star, i) => {
+    star.src = i < selectedRating ? "./img/star_pink.png" : "./img/star_empty.png";
+  });
 }
 
-// Fonction qui permet de réinitialiser les étoiles
+// Fonction pour réinitialiser la sélection
 function handleReset() {
+  // Réinitialise la note sélectionnée et les étoiles
   selectedRating = 0;
-  stars.forEach((star) => {
+  stars.forEach(star => {
     star.src = "./img/star_empty.png";
   });
 }
 
-// Ajouter des écouteurs d'événements pour chaque étoile
+// Ajoute des écouteurs d'événements aux étoiles et au bouton de réinitialisation
 btnReset.addEventListener("click", handleReset);
 stars.forEach((star, index) => {
   star.addEventListener("mouseover", () => handleMouseOver(index));
